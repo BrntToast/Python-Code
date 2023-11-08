feb6a58e0685dd4ffebda4db346ed89bbee5fc45
BrntToast <103519638+BrntToast@users.noreply.github.com>
2023-11-08 20:49:30 +0000

    fixed printing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 turnNumber = 1
 
 def checkWin():
@@ -8,11 +8,11 @@ def checkWin():
     columns = [''.join(x) for x in zip(*board_string)]
     diagonals = [board_string[0][0] + board_string[1][1] + board_string[2][2], board_string[0][2] + board_string[1][1] + board_string[2][0]]
     all = ''.join(rows)
-    if '111' in rows or '111' in columns or '111' in diagonals:
-        return 1
-    elif '222' in rows or '222' in columns or '222' in diagonals:
-        return 2
-    elif '0' not in all:
+    if 'XXX' in rows or 'XXX' in columns or 'XXX' in diagonals:
+        return 'X'
+    elif 'OOO' in rows or 'OOO' in columns or 'OOO' in diagonals:
+        return 'O'
+    elif ' ' not in all:
         return 0
     else:
         return -1
@@ -22,13 +22,12 @@ def setUp():
     while(checkWin() == -1):
         takeTurn()
         displayBoard()
-    print()
+    print(checkWin())
     
 
 def displayBoard():
     for x in board:
-        #line = map(str, ["O" if i == 1 else if "X" for i in x])
-        #print(' | '.join(line))
+        print(' | '.join(x))
         print('_________')
 def takeTurn():
     global turnNumber
@@ -36,9 +35,9 @@ def takeTurn():
         print("Choose a space, ie (1,2). Top right is (1,1)")
         command = [x - 1 for x in map(int, input().split(","))]
         space = board[command[0]][command[1]]
-        if space == 0:
+        if space == ' ':
             print(turnNumber)
-            board[command[0]][command[1]] = 2 if (turnNumber % 2 == 0) else 1
+            board[command[0]][command[1]] = 'O' if (turnNumber % 2 == 0) else 'X'
             turnNumber += 1
             break
 
